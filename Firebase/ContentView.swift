b1dab563bfb6cf1a909f254b56cdd5eddbdcd7d2
Etienne Vautherin <etienne@vautherin.com>
2022-02-08 10:28:06 +0100

    Initial Commit

---
+++ Firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase
+//
+//  Created by Etienne Vautherin on 08/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase/FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseApp.swift
+//  Firebase
+//
+//  Created by Etienne Vautherin on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
